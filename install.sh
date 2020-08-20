c11085d4584fa0ea90b5b44cfd912fdec922c05d
Tristan Smith <tas09914@gmail.co>
2020-08-20 01:13:35 -0400

    Implemented install bash script for server deployment

---
+++ install.sh
@@ -0,0 +1,15 @@
+echo ""
+echo "Setting up \"Game Description\" Component for Steam FEC"
+echo ""
+
+echo ""
+echo "  Cloning Repository"
+git clone https://github.com/hrr47-FEC-Bailey/game-description
+
+echo ""
+echo "  Installing Dependancies"
+npm i
+
+echo ""
+echo "  Building Webpack"
+npm run-script build
